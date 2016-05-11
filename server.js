d66002284f0bb5e3be581dc8afaac77eb310a6f0
kg <kjg310@gmail.com>
2016-05-11 19:05:20 -0400

    for ahmed

--- server.js
+++ server.js
@@ -13,31 +13,93 @@ var PORT = process.env.PORT || 80;
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 
+// Sets JSON Web Token Secret for Encryption
+app.set('jwtSecret', "CODINGROCKS"); // Can put anything in here
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(express.static('public'));
 
-app.post('/auth',function(req,res){
+
+app.post('/auth',function(req,res, next){
 
 	console.log(req.body)
-	var input = 'SELECT * FROM teachers WHERE name="'+req.body.username+'";'
+	var input = 'SELECT * FROM teachers WHERE name="'+ req.body.username+'";'
 	connection.query(input, function(err,result){
-		if (req.body.password == result[0].password) {
-			res.json({success: true})
+
+		// console.log(result)
+
+		if (result == []) {
+			res.json({success: false, message: 'Authentication failed. User not found.'})
 		} else {
-			res.json({success: false})
+
+			if (req.body.password == result[0].password) {
+
+				var token = jwt.sign(result[0], app.get('jwtSecret'), {
+		          expiresIn: '1440m' // expires in 24 hours
+		        });
+
+		        res.json({
+		          success: true,
+		          message: 'Enjoy your token!',
+		          token: token
+		        });
+
+		        console.log("/teacher?token=" + token);
+
+			} else {
+				res.json({success: false, message: 'Authentication failed. Password Incorrect.'})
+			}	
+
 		}
-		console.log(result);
+
 		if (err) throw err;
+
 	});
 
 });
 
+app.all('*', function(req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
+    // If a token is given...
+    if (token){
+
+    	// Attempt to verify it against hte app's jwtSecret (In our case: "CODINGROCKS")
+        jwt.verify(token, app.get('jwtSecret'), function(err, decoded){
+
+        	// If an error is experienced log it.
+            if(err){
+                console.log("Uh... You call that a token? Hah!");
+                return res.json({success: false, message: 'Uh... You call that a token? Hah!'});
+            
+            // If the token is valid let the user know!
+            } else {
+                // if token is valid, save to request for use in other routes
+                console.log("Token looks legit to me. Have fun with our API!");
+
+                // Save that in the requests' "decoded" property so its kept for future use
+                req.decoded = decoded;
+
+                // Proceed to check out the API routes. 
+                next();
+            }
+        });
+
+    // If no token is provided also let the user know!
+    } else {
+        // no token provided
+        return res.status(403).send({
+            success: false,
+            message: "Bro. Did you even send me a token?"
+        });
+    }
+});
 
 app.get('/teacher',function(req,res){
+	console.log("BLAH");
 	res.sendFile(path.join(__dirname+'/public/mat.html'));
 });
 
@@ -45,9 +107,9 @@ app.get('/student',function(req,res){
 	res.sendFile(path.join(__dirname+'/public/student.html'));
 });
 
-app.get('/student',function(req,res){
-	res.sendFile(path.join(__dirname+'/public/index.html'));
-});
+// app.get('/student',function(req,res){
+// 	res.sendFile(path.join(__dirname+'/public/index.html'));
+// });
 
 app.get('/api/teacher',function(req,res){
 
@@ -59,7 +121,7 @@ app.get('/api/teacher',function(req,res){
 		res.json(result);
 	});
 
-	console.log(moment('2010-10-20 12:10:30').isAfter('2010-01-01 12:10:30'));
+	// console.log(moment('2010-10-20 12:10:30').isAfter('2010-01-01 12:10:30'));
 
 	
 });
